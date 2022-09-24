898c4baabca7817e450033074fb0764a844b4493
sqwy <35087732+sqwy@users.noreply.github.com>
2022-09-23 23:03:46 -0400

    test

---
+++ Server.py
@@ -0,0 +1,35 @@
+from socket import *
+
+if __name__ == '__main__':
+    # 服务器端口
+    serverPort = 11000
+    #AF_INET代表ipv4协议，SOCK_STREAM代表TCP协议
+
+    listenSocket = socket(AF_INET, SOCK_STREAM)
+    # ‘0.0.0.0’ 或者‘’都代表不限连接端口
+    listenSocket.bind(('0.0.0.0', serverPort))
+    # listen中的数字表示最多能够接受几个等待中的服务
+    listenSocket.listen(1)
+    print('server started')
+    # True代表无限循环，直到服务器关闭
+    connectionSocket, addr = listenSocket.accept()
+    print('connected to client')
+    while True:
+        print('cycle start')
+        # 从客户端接收信息
+        message = connectionSocket.recv(2048)
+        print('message received')
+        # 解码接收到的信息并将其变成大写
+        capitalizedSentence = message.decode().upper()
+        print('message operated')
+        # 将处理过后的信息加密后发回给客户端
+        connectionSocket.send(capitalizedSentence.encode())
+        print('message sent back to client')
+        # 输出处理信息的内容
+        print('server handled: ' + str(addr) + ' with message:' + message.decode())
+        # 如果接收到退出信息则退出循环并关闭服务器
+        if capitalizedSentence == 'QUIT' or not capitalizedSentence:
+            break
+
+    connectionSocket.close()
+
---
+++ TCPclient.py
@@ -0,0 +1,28 @@
+from socket import *
+
+
+if __name__ == '__main__':
+    # 必须是localhost（为什么？）
+    serverName = 'localhost'
+    # serverPort 一定要和server的serverPort数字一致，否则无法建立连接
+    serverPort = 11000
+    #AF_INET代表ipv4协议，SOCK_STREAM代表TCP协议
+    clientSocket = socket(AF_INET,SOCK_STREAM)
+    #与服务器建立连接
+    clientSocket.connect((serverName,serverPort))
+    # 输入要发送给服务器的信息
+    while True:
+        message = input('Input sentence:')
+        # 将编码后的信息发送给服务器
+        clientSocket.send(message.encode())
+        print('message sent')
+        # 接受从服务器发回的信息
+        modifiedSentense = clientSocket.recv(2048)
+        print('message received')
+        receivedMessage = modifiedSentense.decode()
+        if receivedMessage == 'QUIT' or not receivedMessage:
+            break
+        # 将从服务器收到的信息解码并输出
+        print('From Server:' + receivedMessage)
+        # 关闭服务器
+    clientSocket.close()
